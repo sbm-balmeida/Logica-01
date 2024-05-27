f8e0aaa7d540c3b56b0ab7398e0ba690b6743a47
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-05-27 15:13:17 -0300

    Criando os pontos do jogo

--- ator.js
+++ ator.js
@@ -1,6 +1,7 @@
-let xAtor = 100; 
+let xAtor = 85; 
 let yAtor = 366;
 let colisao = false;
+let meusPontos = 0;
 
 function mostraAtor() {
     image(imagemDoAtor, xAtor, yAtor, 30, 30);
@@ -20,11 +21,25 @@ function verificaColisao() {
     for(let i = 0; i < imagemCarros.length; i++) {
         colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro,alturaCarro, xAtor, yAtor, 15);
         if(colisao) {
-            colidiu();
+            voltaAtorParaPosicaoInicial();
         }
     }
 }
 
-function colidiu() {
+function voltaAtorParaPosicaoInicial() {
     yAtor = 366;
+}
+
+function incluiPontos() {
+    textAlign(CENTER);
+    textSize(25);
+    fill(color(255, 240, 0));
+    text(meusPontos, width/5, 27);
+}
+
+function marcaPonto() {
+    if(yAtor < 15) {
+        meusPontos += 1;
+        voltaAtorParaPosicaoInicial();
+    }
 }
